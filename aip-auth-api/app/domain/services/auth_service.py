ac0f74d1f6610ebf8a4b03fedc2980b4a3e7ac89
mbortnowski <bortnowskim@gmail.com>
2025-04-11 13:53:10 +0200

    Delete aip-auth-api directory

--- aip-auth-api/app/domain/services/auth_service.py
+++
@@ -1,8 +0,0 @@
-from app.ports.auth.vault_service import VaultServicePort
-
-class AuthService:
-    def __init__(self, vault_service: VaultServicePort):
-        self.vault_service = vault_service
-    
-    async def verify_user_permission(self, user_id: str, resource: str, action: str) -> bool:
-        return await self.vault_service.verify_permission(user_id, resource, action)
